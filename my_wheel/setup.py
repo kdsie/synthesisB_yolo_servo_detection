cdcec2069f83bfcbb729c7aa3df3c023bb076695
eloise <2175863610@qq.com>
2026-06-05 18:48:47 +0800

    sync full

---
+++ my_wheel/setup.py
@@ -0,0 +1,24 @@
+﻿from setuptools import Extension, setup
+import numpy as np
+import pybind11
+
+ext_modules = [
+    Extension(
+        "filter_module",
+        ["fliter.cpp"],
+        include_dirs=[pybind11.get_include(), np.get_include()],
+        language="c++",
+        extra_compile_args=["-std=c++11"],
+    )
+]
+#setup
+setup(
+    name="image_filter", 
+    version="0.1.0",
+    author="project team",
+    author_email="team@example.com",
+    description="A 5x5 image convolution module for performance comparison",
+    ext_modules=ext_modules,
+    install_requires=["numpy<2", "pybind11"],
+    python_requires=">=3.8",
+)
